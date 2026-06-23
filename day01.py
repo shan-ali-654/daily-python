bd06f38fc3c169e48346215946444a6b85f7b2ca
Shan Ali <alizeeshan1024@gmail.com>
2026-06-23 23:31:50 +0500

    Data Types Done

--- day01.py
+++ day01.py
@@ -1,2 +1,29 @@
 name = "Ali"
 print(f"Day 1 — {name} is shipping daily now.")
+
+quantity = 5
+print (f"{quantity}, Type:  {type(quantity)}")
+
+unit_price = 99.50 
+print (f"{unit_price}, Type:  {type(unit_price)}")
+
+big_num = 50_000_000
+print(f"Number is : {big_num}")
+
+first = "Ali"
+last = "Jatt"
+full_name = first + " " + last
+print(f"Full Name is :{full_name}")
+print(f"First 3 letter is :{full_name[0:4]}")
+
+is_paid = True
+if  is_paid:
+    print("Paid")
+else:
+    print("Unpaid")
+
+
+discount_code = None
+if discount_code is None:
+    print("No Discount")
+
