67c394603d1927292248b101299bd650d2375953
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-25 00:29:37 +0500

    chk

--- collections_02.py
+++ collections_02.py
@@ -1,64 +1,64 @@
-# # Lists
+# Lists
 
-# fruit_names = ["Apple", "Grapes", "Banana", "Orange"]
-# print(fruit_names)
+fruit_names = ["Apple", "Grapes", "Banana", "Orange"]
+print(fruit_names)
 
-# print(fruit_names[0])
-# print(fruit_names[-1])
+print(fruit_names[0])
+print(fruit_names[-1])
 
-# fruit_names.append("Mango")
-# print(fruit_names)
+fruit_names.append("Mango")
+print(fruit_names)
 
-# fruit_names[1] = "Pineapple"
-# print(fruit_names)
+fruit_names[1] = "Pineapple"
+print(fruit_names)
 
-# print(len(fruit_names))
+print(len(fruit_names))
 
-# # Tuples
+# Tuples
 
-# num = (20, 30, 40)
-# print(num)
+num = (20, 30, 40)
+print(num)
 
-# num[0] = 23
-# print(num) # Showing an {TypeError: 'tuple' object does not support item assignment}
+num[0] = 23
+print(num) # Showing an {TypeError: 'tuple' object does not support item assignment}
 
-# person_x = ("Zeeshan", 23 , "N/A")
-# name, age, job = person_x
-# print(name)
-# print(age)
-# print(job)
+person_x = ("Zeeshan", 23 , "N/A")
+name, age, job = person_x
+print(name)
+print(age)
+print(job)
 
-# # Dicts
+# Dicts
 
-# product_x = {"name": "Banana", "price":200, "in_Stock": True}
-# print(product_x)
+product_x = {"name": "Banana", "price":200, "in_Stock": True}
+print(product_x)
 
-# print(f"Price is {product_x['price']}")
+print(f"Price is {product_x['price']}")
 
-# product_x["quantity"] = 25
-# print(product_x.get("name"))
-# print(product_x.get("colour"))
-# print(product_x.get("price"))
-# product_x["price"] = 250
-# print(product_x)
+product_x["quantity"] = 25
+print(product_x.get("name"))
+print(product_x.get("colour"))
+print(product_x.get("price"))
+product_x["price"] = 250
+print(product_x)
 
-# # Sets
+# Sets
 
-# nums = {2, 3, 2, 6, 4, 3, 5}
-# print(nums) # it prints values but every time those order get changed also remove repeated numbers/items iteself  
+nums = {2, 3, 2, 6, 4, 3, 5}
+print(nums) # it prints values but every time those order get changed also remove repeated numbers/items iteself  
 
-# nums.add(9)
-# print(nums)
+nums.add(9)
+print(nums)
 
-# num_a = {2, 3, 5, 6}
-# num_b = {2, 4, 5, 7}
+num_a = {2, 3, 5, 6}
+num_b = {2, 4, 5, 7}
 
-# print(f"Union is : {num_a.union(num_b)}")
-# print(f"Intersection is : {num_a.intersection(num_b)}")
-# print(f"Difference is : {num_a.difference(num_b)}")
-# print(f"Difference is : {num_b.difference(num_a)}")
+print(f"Union is : {num_a.union(num_b)}")
+print(f"Intersection is : {num_a.intersection(num_b)}")
+print(f"Difference is : {num_a.difference(num_b)}")
+print(f"Difference is : {num_b.difference(num_a)}")
 
-# # Casting
+# Casting
 num_str = '25'
 num_int = int(num_str)
 print(f"{num_int} Type : {type(num_int)}")
