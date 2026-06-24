c11c7d899ee7887df4dbb509b734c9bf0b913b14
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-24 18:25:15 +0500

    Done Dicts

--- collections_02.py
+++ collections_02.py
@@ -21,3 +21,15 @@ print(num)
 
 num[0] = 23
 print(num) # Showing an {TypeError: 'tuple' object does not support item assignment}
+
+# Dicts
+
+product_x = {"name": "Banana", "price":200, "in_Stock": True}
+print(product_x)
+
+print(f"Price is {product_x["price"]}")
+
+product_x["quantity"] = 25
+product_x["price"] = 250
+print(product_x)
+
