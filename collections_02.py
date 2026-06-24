c54dad0ce9d6a8dc3fa9722a0c6e2840d0254fd0
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-24 16:19:58 +0500

    Done List

---
+++ collections_02.py
@@ -0,0 +1,15 @@
+# Lists
+
+fruit_names = ["Apple", "Grapes", "Banana", "Orange"]
+print(fruit_names)
+
+print(fruit_names[0])
+print(fruit_names[-1])
+
+fruit_names.append("Mango")
+print(fruit_names)
+
+fruit_names[1] = "Pineapple"
+print(fruit_names)
+
+print(len(fruit_names))
