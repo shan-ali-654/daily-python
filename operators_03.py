83dd2dd8c3ab84c746328e1b24160d598d4afc11
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-26 22:53:31 +0500

    Done Arithmetic

--- operators_03.py
+++ operators_03.py
@@ -17,3 +17,16 @@ print(f"Decerease : {x}")
 x *= x
 print(f"Doubel : {x}")
 
+# Arithmetic
+num_1, num_2 = 17, 5
+print(f"Sum is : {num_1 + num_2}")
+print(f"Difference is : {num_1 - num_2}")
+print(f"Product is  : {num_1 * num_2}")
+
+num_1, num_2 = 17, 5
+print(f"Dividing is : {num_1/num_2}")
+print(f"Whole Number is : {(num_1/num_2):.0f}")
+
+x, y = 15, 20
+print(x == y)
+
