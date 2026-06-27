24feaf6b6567334267fd0857807def29276dd637
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-27 23:13:26 +0500

    Done Range()

--- loops_04.py
+++ loops_04.py
@@ -7,7 +7,7 @@ for city in city_names:
     print(city)
 
 # Q2
-# print each word
+# print each letter
 word = 'Normal'
 for letter in word:
     print(letter)
@@ -23,3 +23,14 @@ for key, value in person.items():
     print(f"Key is {key} : Value is {value}")
 
 # Range
+
+# Q4
+# print num from 1 to 10
+for num in range(1, 11):
+    print(num)
+
+# Q5
+# Print Only Even Num 
+for num in range (1, 21):
+    if num % 2 == 0 :
+        print(f"Even Number is : {num}")
