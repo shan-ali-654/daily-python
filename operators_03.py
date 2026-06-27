21761aaa9863452785830003318e9633b1967d5a
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-27 08:23:30 +0500

    Done Inc Q20

--- operators_03.py
+++ operators_03.py
@@ -91,17 +91,19 @@ print(not x)
 
 # Q18
 x , y= 7, 9
-num = x == y
-if num == True:
+num = x != y
+if num == False:
     print("They are Equal.")
 else:
     print("They are not Equal.")
 
 # Q19
 score = 50
-if score <= 50:
+if score >= 50:
     print(f"Your score is {score}")
 else:
-    print('Not Right.')
+    print('Low Score.')
 
 # Q20
+x, y = 8, 10
+print(x <= y)
