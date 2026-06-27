1c634c25bce73a228932d652e3f9480d61f6215d
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-27 22:21:14 +0500

    Done For Loops

---
+++ loops_04.py
@@ -0,0 +1,20 @@
+# For Loops
+
+# Q1
+city_names = ['Lahore', 'Karachi', 'Sialkot', 'Gawadar', ]
+for city in city_names:
+    print(city)
+
+# Q2
+word = 'Normal'
+for w in word:
+    print(w)
+
+# Q3
+person = {
+    'name' : 'Shan',
+    'age' : 23,
+    'City' : 'Lahore'
+}
+for key, value in person.items():
+    print(f"Key is {key} : Value is {value}")
