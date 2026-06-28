c83f2e5369ab7779f4f57f34b3c7bf974b85b123
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-28 22:20:48 +0500

    Fix Q1

--- conditional_statements_05.py
+++ conditional_statements_05.py
@@ -4,13 +4,13 @@
 exam_score = 55
 if exam_score >= 90:
     print('Grade is A')
-elif exam_score >= 80 and exam_score <= 89:
+elif exam_score >= 80:
     print("Grade is B")
-elif exam_score >= 70 and exam_score <= 79:
+elif exam_score >= 70:
     print("Grade is C")
-elif exam_score >= 60 and exam_score <= 69:
+elif exam_score >= 60 :
     print("Grade is D")
-elif exam_score <= 60 :
+elif exam_score < 60 :
     print("Grade is F")
 else:
     print("Kindly proceed with Number")
