28d932ca4ad7bb20273fd0da4f14e4a60e8b7e3d
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-28 05:30:44 +0500

    Fix Q13, 14

--- loops_04.py
+++ loops_04.py
@@ -96,12 +96,13 @@ even_num = []
 for num in range(1 , 11):
     if num % 2 == 0:
         even_num.append(num)
-        print(even_num)
+print(even_num)
         
 # Loops Else (try it )
 
 # Q14
 for num in range(1, 6):
     print(num)
-print("It's Done.")
+else:
+    print("It's Done.")
 
