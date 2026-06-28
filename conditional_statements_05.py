555380cc45ae1b53ea4cb4cc8031e361b149c860
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-28 22:10:37 +0500

    Done Q1

---
+++ conditional_statements_05.py
@@ -0,0 +1,16 @@
+# Q1
+
+# build Grade marking 
+exam_score = 55
+if exam_score >= 90:
+    print('Grade is A')
+elif exam_score >= 80 and exam_score <= 89:
+    print("Grade is B")
+elif exam_score >= 70 and exam_score <= 79:
+    print("Grade is C")
+elif exam_score >= 60 and exam_score <= 69:
+    print("Grade is D")
+elif exam_score <= 60 :
+    print("Grade is F")
+else:
+    print("Kindly proceed with Number")
