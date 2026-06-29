0acdb7518bd36173f66a09d22a20ba121d884d26
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-29 05:20:26 +0500

    Fix Q3

--- conditional_statements_05.py
+++ conditional_statements_05.py
@@ -18,11 +18,11 @@ else:
 # Q3
 # Found max number without max function 
 num_1, num_2, num_3 = 4, 9, 8
-if num_1 > num_2:
+if num_1 > num_2 and num_1 > num_3:
     print(f"Greater Num is '{num_1}' 1st Number.")
-elif num_2 > num_3:
+elif num_2 > num_3 and num_2 > num_1:
         print(f"Greater Num is '{num_2}' 2nd Number.")
-elif num_3 > num_1:
+elif num_3 > num_1 and num_3 > num_2:
         print(f"Greater Num is '{num_3}' 3rd Number.")
 else:
      print("Wrong Input Kindly proceed with Numbers. ")
