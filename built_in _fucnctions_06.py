56caa5c2312719d20053c12566de3d8ff56886bd
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-29 23:05:34 +0500

    Done Q1

---
+++ built_in _fucnctions_06.py
@@ -0,0 +1,14 @@
+# Q1
+# Orderd Totals
+totals = [1200, 450, 9800, 300]
+total_revenue = sum(totals)
+print(f"Total Revenue is : {total_revenue}.")
+
+order_count = len(totals)
+print(f"Order Count is : {order_count}")
+
+bigger_num = max(totals)
+print(f"Bigger Number is : {bigger_num}.")
+
+lower_num = min(totals)
+print(f"Lower Number is : {lower_num}")
