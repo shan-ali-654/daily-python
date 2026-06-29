b2500074561237fe6e5defb12783b4b4bcfea931
Shan Ali <zeeshan.me.jutt@gmail.com>
2026-06-29 23:45:45 +0500

    Change File Name

---
+++ built_in_fucnctions_06.py
@@ -0,0 +1,34 @@
+# Q1
+# Orderd Totals
+totals = [1200, 450, 9800, 300]
+total_revenue = sum(totals)
+print(f"Total Revenue is : {total_revenue}.")
+
+order_count = len(totals)
+print(f"Order Count is : {order_count}")
+
+bigger_num = max(totals)
+print(f"Bigger Number is : {bigger_num}.")
+
+lower_num = min(totals)
+print(f"Lower Number is : {lower_num}")
+
+# Q2
+num  = 9
+string = 'Apple'
+lst = [12, 'Fruit']
+print(f"Type of Data is : {type(num)}.")
+print(f"Type of Data  is : {type(string)}.")
+print(f"Type of Data is : {type(lst)}.")
+
+# Q3
+lst = [1200, 450, 9800, 300]
+sort_lst = sorted(lst)
+rev_sort_lst = sorted(lst, reverse=True)
+print(sort_lst)
+print(rev_sort_lst)
+
+# Q4: 
+word_lst = ["Python", "Programming", "Code", "Functions"]
+longest_word = max(word_lst, key=len)
+print(longest_word)
